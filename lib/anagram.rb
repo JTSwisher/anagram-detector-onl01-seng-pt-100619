b2b5b34fd23440c28c63f3ddfa9e575c8adc7dd1
Jeff Swisher <jefftswisher@gmail.com>
2019-11-02 19:33:51 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,7 +11,7 @@ class Anagram
   def match(possible_anagrams)
     sorted_words = []
     possible_anagrams.select do |word_match|
-      word_match.split("").sort == 
+      word_match.split("").sort == @word.spli("").sort 
     end 
   end 
   
